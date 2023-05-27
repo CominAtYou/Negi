9fb08a35fd85e21e74eeb5cede8234c43186dde4
William M <cominatyou@cominatyou.com>
2023-05-26 20:12:04 -0500

    Bump material to 1.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ dependencies {
     implementation("commons-codec:commons-codec:1.15")
     implementation("androidx.biometric:biometric:1.1.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.android.material:material:1.9.0")
     implementation("com.google.code.gson:gson:2.8.9")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
